48b36cdd9f52286be80e4b2800ec490e2dd375c4
lion-temp <liontemp@MacBook-de-lion-test.local>
2020-09-03 22:54:46 +0100

    Change on provider in order to make db work on heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,29 @@
 <?php
 
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+//import this
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider{
+
     /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
-    {
-        //
+    public function register(){
+        //add this
+        Schema::defaultStringLength(191);
     }
-
+    
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot(){
         //
     }
 }
